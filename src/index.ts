7601c9cd1afea71700f39c19de49549a6de33624
sklvv <iisokolov1@gmail.com>
2022-11-09 00:00:56 +0500

    feat: add slider

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import App from "./app";
-import { setupForm } from "./features/addNewHero";
+import { Form } from "./features/addNewHero";
+import { IHero, Slider } from "./features/heroesSlider";
+
+// Иньектим базову разметку
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `${App()}`;
-setupForm(document.querySelector<HTMLDivElement>("#form")!);
+
+// Пустой массив заменяет стор и в этом случае еще и бек с бд
+
+export const heroesArr: IHero[] = [];
+
+// Добавляем слайдер
+
+Slider(document.querySelector<HTMLDivElement>("#slider")!, heroesArr);
+
+// Добавляем форму
+
+Form(document.querySelector<HTMLDivElement>("#form")!);
